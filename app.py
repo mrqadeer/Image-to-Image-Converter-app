b7b245d7f4c95b867e945ad86c0d9bd9f10d24c1
mrqadeer <mrqadeer1231122@gmail.com>
2024-02-21 00:01:50 +0500

    working app

--- app.py
+++ app.py
@@ -30,44 +30,43 @@ def main():
         if 'username' not in st.session_state:
             st.session_state.username=""
         # st.markdown("<h4>Made by Qadeer</h4>",unsafe_allow_html=True)
-        submit=st.button("Submit")
-        if submit:
+
             
-            
-            if (file is not None) and ((len(username)>=3) and validate_name(username)) :
-                st.success(f"Welcome dear {username.title()}")
-                formats = ["JPG", "JPEG", "PNG","WEBP","TIF","GIF"]
-                selected = st.selectbox("Select format", formats)
+    if (file is not None) and ((len(username)>=3) and validate_name(username)) :
+        st.success(f"Welcome dear {username.title()}")
+        formats = ["JPG", "JPEG", "PNG","WEBP","TIF","GIF"]
+        selected = st.selectbox("Select format", formats)
         
-                name=file.name.split(".")[0]
-                cols=st.columns(2)
-                with cols[0]:
-                    st.markdown("### Orignal Image")
-                    st.image(file)
-                convert = st.button("Convert")
-                if convert:
-                    copy_to_project_folder(file)
-                    file_path = f"images/{file.name}"
-                    with st.spinner("Converting"):
-                        converted_file_path = converter(file_path, selected)
-                        st.success(f"Dear {username} your file {file.name} is converted.")
-                        st.balloons()
+        name=file.name.split(".")[0]
+        cols=st.columns(2)
+        with cols[0]:
+            st.markdown("### Orignal Image")
+            st.image(file)
+        convert = st.button("Convert")
+        if convert:
+            
+            copy_to_project_folder(file)
+            file_path = f"images/{file.name}"
+            with st.spinner("Converting"):
+                converted_file_path = converter(file_path, selected)
+                st.success(f"Dear {username} your file {file.name} is converted.")
+                st.balloons()
                     
-                    with cols[1]:
-                        st.markdown("### Converted Image")
-                        # Display converted image
-                        st.image(converted_file_path)
+            with cols[1]:
+                st.markdown("### Converted Image")
+                    # Display converted image
+                st.image(converted_file_path)
 
                     # Download button for the converted image
-                    with open(converted_file_path, "rb") as file:
-                        converted_image_bytes = file.read()
-                    st.download_button(
-                        label="Download Converted Image",
+            with open(converted_file_path, "rb") as file:
+                converted_image_bytes = file.read()
+                st.download_button(
+                label="Download Converted Image",
                         data=converted_image_bytes,
                         file_name=f"converted_{name}.{selected.lower()}",
                         mime="image"
                     )
                     
-            else:st.warning("Complete the setup.")
+    else:st.warning("Complete the setup to proceed!")
 if __name__ == "__main__":
     main()
